196d81dfb8107cc688a39f173e77b1570dbce666
kal-kyokya <kalkyokya4@gmail.com>
2025-05-27 20:36:15 +0000

    Add 'Swagger' to autogenerate Swagger documentation.

--- alx_travel_app/alx_travel_app/urls.py
+++ alx_travel_app/alx_travel_app/urls.py
@@ -1,4 +1,4 @@
-"""
+'''
 URL configuration for alx_travel_app project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -13,10 +13,27 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+'''
 from django.contrib import admin
 from django.urls import path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title='ALX Travel App API',
+        default_version='v1',
+        description='API documentation for ALX Travel App',
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('swagger/',
+         schema_view.with_ui('swagger', cache_timeout=0),
+         name='schema-swagger-ui'),
 ]
